f29ea7d3979ec2229972fc0312ec28381134b13b
jagathda <mail.jagath@gmail.com>
2024-09-09 11:50:54 +0300

    Created public subnets

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ resource "aws_vpc" "fargate_vpc" {
     Name = "FargateVPC"
   }
 }
+
+# Created public subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.fargate_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.fargate_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-north-1b"
+}
